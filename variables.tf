b4402bb5e6fc18b151df80d1bded4268ac786b81
miguel-hdez-hashicorp <miguel.hernandez1@hashicorp.com>
2024-05-22 08:08:30 +0200

    adding files

--- variables.tf
+++ variables.tf
@@ -1,18 +1,50 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+##############################################################################
+# Variables File
+#
+# Here is where we store the default values for all the variables used in our
+# Terraform code. If you create a variable with no default, the user will be
+# prompted to enter it (or define it via config file or command line flags.)
+
+variable "prefix" {
+  description = "This prefix will be included in the name of most resources."
+}
 
 variable "region" {
-  description = "AWS region"
-  default     = "us-west-1"
+  description = "The region where the resources are created."
+  default     = "us-east-1"
+}
+
+variable "address_space" {
+  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_prefix" {
+  description = "The address prefix to use for the subnet."
+  default     = "10.0.10.0/24"
 }
 
 variable "instance_type" {
-  description = "Type of EC2 instance to provision"
+  description = "Specifies the AWS instance type."
   default     = "t2.micro"
 }
 
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
+variable "admin_username" {
+  description = "Administrator user name for mysql"
+  default     = "hashicorp"
 }
 
+variable "height" {
+  default     = "400"
+  description = "Image height in pixels."
+}
+
+variable "width" {
+  default     = "600"
+  description = "Image width in pixels."
+}
+
+variable "placeholder" {
+  default     = "placekitten.com"
+  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
+}
